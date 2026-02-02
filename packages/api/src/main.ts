33f8091740458a2ae23db167c20a105c0da161bf
foundteach-com <mannezdev@gmail.com>
2026-02-02 15:51:37 -0500

    fix: update CORS configuration to allow all origins

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*', // Allow all origins for debugging, or specify ['https://app.ospinacomercializadoraysuministros.com']
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   
   const port = process.env.PORT || 4000;
   await app.listen(port, '0.0.0.0');
